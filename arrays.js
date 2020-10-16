97c036082d17cede77aa4fd85157d91bf0273943
Will Studabaker <will.studabaker@gmail.com>
2020-10-16 16:13:58 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,5 +4,5 @@ function addElementToBeginningOfArray(array, element){
   return newArray.unshift(element)
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
-   array.unshift(element)
+   return array.unshift(element)
 }
