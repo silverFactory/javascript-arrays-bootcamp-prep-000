7d3374a3e5f238f31111a333566c3aec68b66ec4
Will Studabaker <will.studabaker@gmail.com>
2020-10-16 16:15:00 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 function addElementToBeginningOfArray(array, element){
   var newArray = array;
-  return newArray.unshift(element)
+  return newArray.unshift(element);
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
-   return array.unshift(element)
+   return array.unshift(element);
 }
